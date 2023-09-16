af9ff7b46b5ca880e22ed393ffa8e1557ffc404a
Natalia <natalia.lopez.osorio@correounivalle.edu.co>
2023-09-15 19:02:22 -0500

    Etiqueta nombre

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,9 +17,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,10 +59,25 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    val roundedCornerShape: Shape = RoundedCornerShape(20.dp)
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = stringResource(R.string.name),
+            modifier = Modifier
+                .background(color = Color.LightGray,
+                            shape = roundedCornerShape)
+                .padding(16.dp),
+            fontSize = 23.sp,
+            fontWeight = FontWeight.Bold)
+        Text(text = stringResource(R.string.code),
+            modifier = Modifier
+                .background(color = Color(238,237,238),
+                    shape = roundedCornerShape)
+                .padding(10.dp),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold)
         Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
         Button(onClick = { result= (1..6).random()}) {
             Text(stringResource(R.string.roll))
